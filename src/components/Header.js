9e0a4b1436bad6ef00a27e61a95dc79c8eb5d79b
vdalvez <victor.alvez26@hotmail.com>
2025-11-24 19:19:57 -0300

    feat: Se configuran las variables de entorno

--- src/components/Header.js
+++ src/components/Header.js
@@ -395,12 +395,11 @@ const Header = ({ user, activePage = '' }) => {
                 <button
                   type="button"
                   onClick={() => {
-                    console.log(' config.BACKEND_URL', config.BACKEND_URL);
                     const authUrl = new URL('https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize');
                     authUrl.searchParams.set('client_id', '890192');
                     authUrl.searchParams.set('response_type', 'code');
                     authUrl.searchParams.set('scope', 'openid personal_info email');
-                    authUrl.searchParams.set('redirect_uri', config.BACKEND_URL);
+                    authUrl.searchParams.set('redirect_uri', config.CALLBACK_URL);
                     authUrl.searchParams.set('state', Math.random().toString(36).substring(2, 15));
                     window.location.href = authUrl.toString();
                   }} 
